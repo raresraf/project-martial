84319b03bb270c1bf3ae3f6136f60fe06c129416
raresraf <raresfolea97@gmail.com>
2023-01-29 18:27:06 +0200

    upload

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -7,24 +7,43 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DiffComponent } from './diff/diff.component';
 
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import {HttpClientModule} from '@angular/common/http';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DiffComponent
+    DiffComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatGridListModule,
     MatCardModule,
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatProgressBarModule,
+    MatToolbarModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-
 
 
--- ui/src/app/diff/diff.component.ts
+++ ui/src/app/diff/diff.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 export interface Tile {
   color: string;
-  text: string;
+  cols: number;
+  rows: number;
+  text?: string | undefined;
 }
-
 
 @Component({
   selector: 'app-diff',
@@ -12,47 +14,68 @@ export interface Tile {
   styleUrls: ['./diff.component.css']
 })
 export class DiffComponent {
-
   cpp1 = `
- #include <iostream>
+  #include <iostream>
 
- using namespace std;
+  using namespace std;
 
- int main() {
-   int a;
-   // read a.
-   cin >> a;
-   if (a % 2 == 0)
-     // Check if a is even.
-     cout << "even";
-   else
-     // Is odd.
-     cout << "odd";
-
-   return 0;
- }
+  int main() {
+      cout << "Hello World!";
+      return 0;
+  }
   `
+
   cpp2 = `
   #include <iostream>
 
   using namespace std;
-  
+
   int main() {
-    int a;
-    // read a.
-    cin >> a;
-    if (a % 2 == 0)
-      // Check even.
-      cout << "even";
-    else
-      // Check odd.
-      cout << "odd";
-    return 0;
-  }  
+      // With a comment.
+      cout << "Hello World!";
+      return 0;
+  }
   `
 
-  tiles: Tile[] = [
-    {text: this.cpp1, color: '#ffe6e6'},
-    {text: this.cpp2, color: '#dcf0d5'},
+  file_upload_tiles: Tile[] = [
+    { cols: 1, rows: 1, color: '#ffe6e6' },
+    { cols: 1, rows: 1, color: '#dcf0d5' },
   ];
+
+  tiles: Tile[] = [
+    { text: this.cpp1, cols: 1, rows: 10, color: "#FFFEFE" },
+    { text: this.cpp2, cols: 1, rows: 10, color: "#FEFFFE" },
+  ];
+
+
+  fileName: string[] = ["", ""]
+  constructor(private http: HttpClient) { }
+
+  onFileSelected(event, id: number) {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.fileName[id] = file.name;
+      console.log("%s", this.fileName[id])
+      let reader = new FileReader();
+      reader.onload = (evt) => {
+        this.tiles[id].text = evt.target?.result?.toString()
+      }
+      reader.readAsBinaryString(file);
+
+      // TODO(raresraf): Upload them to backend!
+
+      // const formData = new FormData();  
+      // formData.append("thumbnail", file);  
+      // const upload$ = this.http.post("/api/thumbnail-upload", formData);  
+      // upload$.subscribe();  
+    }
+  }
+
+  onFileSelected0(event) {  
+    this.onFileSelected(event, 0)
+  }
+
+  onFileSelected1(event) {  
+    this.onFileSelected(event, 1)
+  }
 }

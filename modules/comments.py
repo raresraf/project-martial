c8253bd12a999b30827aca2c906f7d564870b5ab
raresraf <raresfolea97@gmail.com>
2023-03-26 18:32:22 +0300

    better comments module

--- modules/comments.py
+++ modules/comments.py
@@ -6,13 +6,14 @@ import datetime
 import re
 from rapidfuzz import fuzz
 
+
 class CommentsAnalysis():
     def __init__(self):
         self.initTimestamp = datetime.datetime.now()
         self.fileDict = {}
 
     def load_file(self, filepath):
-        f = open(filepath,'r')
+        f = open(filepath, 'r')
         source = f.read()
         self.fileDict[filepath] = source
 
@@ -20,13 +21,41 @@ class CommentsAnalysis():
         self.fileDict[filepath] = source
 
     def analyze(self):
+        pattern = re.compile(r'''(?x)
+(?=["'/])      # trick to make it faster, a kind of anchor
+(?:
+    "(?=((?:[^"\\?]+|\?(?!\?/)|(?:\?\?/|\\)[\s\S])*))\1" # double quotes string
+  |
+    '(?=((?:[^'\\?]+|\?(?!\?/)|(?:\?\?/|\\)[\s\S])*))\2' # single quotes string
+  |
+    (
+        /(?:(?:\?\?/|\\)\n)*/(?:.*(?:\?\?|\\)/\n)*.* # single line comment
+      |
+        /(?:(?:\?\?/|\\)\n)*\*                       # multiline comment
+        (?=((?:[^*]+|\*+(?!(?:(?:\?\?/|\\)\n)*/))*))\4
+        \*(?:(?:\?\?/|\\)\n)*/             
+    )
+)
+''')
+
         findings_dict = {}
         for k, v in self.fileDict.items():
-            pattern = re.compile('(?:/\*(.*?)\*/)|(?://(.*?)\n)',re.S)
             findings = pattern.findall(v)
             findings_dict[k] = []
             for f in findings:
-                findings_dict[k].append(f[1])
+                for val in f[2:]:
+                    val = val.split("\n")
+                    for sval in val:
+                        sval = sval.lstrip(" ").rstrip(" ").strip("\t").strip("\n")
+                        if sval == '':
+                            continue
+                        if sval == '//':
+                            continue
+                        if sval == '/*':
+                            continue
+                        if sval == '*/':
+                            continue
+                        findings_dict[k].append(sval)
         return findings_dict
 
     def analyze_2_files(self):
@@ -39,7 +68,6 @@ class CommentsAnalysis():
     def analyze_2_files_fuzzy(self):
         return self.analyze_2_files_fuzzy_impl()
 
-
     def analyze_2_files_fuzzy_impl(self):
         ret = []
         findings_dict = self.analyze()
@@ -47,13 +75,13 @@ class CommentsAnalysis():
         file2 = self.comms_to_seq(findings_dict["file2"])
         for f1 in file1:
             for f2 in file2:
-                if (fuzz.ratio(f1[0],f2[0])) > 96.66:
+                if (fuzz.ratio(f1[0], f2[0])) > 96.66:
+                    print(f1, f2)
                     ret = ret + f1[1] + f2[1]
-        print(ret)
         return ret
 
-
     """ Max 10 long sequences, consecutive."""
+
     def seq(self, x):
         if len(x) == 0:
             return []
@@ -63,7 +91,7 @@ class CommentsAnalysis():
             if n[0] == x[0] + 1 and len(n) < 9:
                 res = res + [(x[0],) + n]
         return res + nexts
-    
+
     def comms_to_seq(self, file):
         l = len(file)
         resp = []
@@ -71,7 +99,7 @@ class CommentsAnalysis():
             coming_from = []
             long_comm = ""
             for ii in i:
-                long_comm = long_comm + re.sub(r'[^\w\s]','',file[ii])
+                long_comm = long_comm + re.sub(r'[^\w\s]', '', file[ii])
                 coming_from.append(file[ii])
             resp.append((long_comm, coming_from))
         return resp

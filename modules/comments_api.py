5923c3104365f7b7100da1b8aafb32a2cdd7a259
raresraf <raresfolea97@gmail.com>
2023-09-03 21:55:14 +0300

    format comments_api

--- modules/comments_api.py
+++ modules/comments_api.py
@@ -4,9 +4,11 @@ from modules.comments import CommentsAnalysis
 import modules.comments as comments
 import modules.comments_helpers as comments_helpers
 
+
 def run(upload_dict):
     ca = common_run(upload_dict)
     return comments_common(ca)
+
 
 def common_run(upload_dict) -> CommentsAnalysis:
     ca = CommentsAnalysis()
@@ -18,12 +20,13 @@ def common_run(upload_dict) -> CommentsAnalysis:
         ca.load_text("file2", upload_dict["file2"])
     return ca
 
+
 def run_text(upload_dict):
     ca = common_run(upload_dict)
     return comments_common(ca, feed_method=feed_common_list_in_text_report)
 
 
-def feed_common_list_in_report(entry, report, token, common_list, lines_in_1, lines_in_2, keep_text = False):
+def feed_common_list_in_report(entry, report, token, common_list, lines_in_1, lines_in_2, keep_text=False):
     report[entry] = []
     list_len = len(common_list)
     print_report = [list_len / 10, 2 * list_len / 10, 3 * list_len / 10, 4 * list_len / 10, 5 * list_len /
@@ -32,8 +35,8 @@ def feed_common_list_in_report(entry, report, token, common_list, lines_in_1, li
         f"[traceID: {token}] common_list for {entry}: found {list_len} common sequences")
     for idx, _ in enumerate(common_list):
         update = {
-                    "file1": lines_in_1[idx],
-                    "file2": lines_in_2[idx],
+            "file1": lines_in_1[idx],
+            "file2": lines_in_2[idx],
         }
         if keep_text:
             update["file1_text"] = str(common_list[idx][0])
@@ -47,27 +50,27 @@ def feed_common_list_in_report(entry, report, token, common_list, lines_in_1, li
 
 
 def feed_common_list_in_text_report(entry, report, token, common_list, lines_in_1, lines_in_2):
-    feed_common_list_in_report(entry, report, token, common_list, lines_in_1, lines_in_2, keep_text = True)
+    feed_common_list_in_report(
+        entry, report, token, common_list, lines_in_1, lines_in_2, keep_text=True)
 
 
-
-def comments_common(ca: CommentsAnalysis, feed_method = feed_common_list_in_report):
+def comments_common(ca: CommentsAnalysis, feed_method=feed_common_list_in_report):
     report = {}
 
     common_list_comment_exact_lines_files, lines_in_1, lines_in_2 = ca.analyze_2_files(
         "Exact match", ca.exact_match_similarity, ca.comm_to_seq_1)
     feed_method("comment_exact_lines_files",
-                               report, ca.token, common_list_comment_exact_lines_files, lines_in_1, lines_in_2)
+                report, ca.token, common_list_comment_exact_lines_files, lines_in_1, lines_in_2)
 
     common_list_comment_fuzzy_lines_files, lines_in_1, lines_in_2 = ca.analyze_2_files(
         "Fuzzy match", ca.fuzzy_similarity, ca.comm_to_seq_6)
     feed_method("comment_fuzzy_lines_files",
-                               report, ca.token, common_list_comment_fuzzy_lines_files, lines_in_1, lines_in_2)
+                report, ca.token, common_list_comment_fuzzy_lines_files, lines_in_1, lines_in_2)
 
     common_list_comment_spacy_core_web_lines_files, lines_in_1, lines_in_2 = ca.analyze_2_files(
         "SpacyCoreWeb match", ca.spacy_similarity, ca.comm_to_seq_doc)
     feed_method("comment_spacy_core_web_lines_files",
-                               report, ca.token, common_list_comment_spacy_core_web_lines_files, lines_in_1, lines_in_2)
+                report, ca.token, common_list_comment_spacy_core_web_lines_files, lines_in_1, lines_in_2)
 
     if ca.enable_elmo:
         common_list_comment_elmo_lines_files, lines_in_1, lines_in_2 = ca.analyze_2_files(
@@ -78,10 +81,7 @@ def comments_common(ca: CommentsAnalysis, feed_method = feed_common_list_in_repo
     report_without_overlap = remove_comments_superset_that_were_not_deduped_before(
         report)
 
-    print(report_without_overlap)
     return report_without_overlap
-
-
 
 
 def remove_comments_superset_that_were_not_deduped_before(report: dict) -> dict:

8e8c1f325b648213be86b45319d46a900833df11
raresraf <raresfolea97@gmail.com>
2024-03-02 10:59:49 +0200

    gt + use

--- scripts/comments_join_files.py
+++ scripts/comments_join_files.py
@@ -3,7 +3,7 @@ import json
 with open('/Users/raresraf/code/project-martial/dataset/ground-truth-comments-6-kubernetes.txt', 'r') as f:
     data_1 = json.load(f)
     
-with open('/Users/raresraf/code/project-martial/dataset/levenshtein-comments-6-kubernetes.txt', 'r') as f:
+with open('/Users/raresraf/code/project-martial/dataset/use-comments-6-kubernetes.txt', 'r') as f:
     data_2 = json.load(f)
 
 data = {}

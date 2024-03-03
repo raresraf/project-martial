97abe6534d060de6837883906d94babd8cce7f8b
raresraf <raresfolea97@gmail.com>
2024-03-03 18:22:06 +0200

    better precision

--- scripts/comments_compute_true_positives.py
+++ scripts/comments_compute_true_positives.py
@@ -9,60 +9,60 @@ from sklearn.metrics import classification_report
  [      16 12817674]]
               precision    recall  f1-score   support
 
- not similar       1.00      1.00      1.00  12817690
-     similar       1.00      0.77      0.87     99146
+ not similar    0.99823   1.00000   0.99912  12817690
+     similar    0.99979   0.77115   0.87071     99146
 
-    accuracy                           1.00  12916836
-   macro avg       1.00      0.89      0.93  12916836
-weighted avg       1.00      1.00      1.00  12916836
+    accuracy                        0.99824  12916836
+   macro avg    0.99901   0.88557   0.93491  12916836
+weighted avg    0.99824   0.99824   0.99813  12916836
 
 * * * NEW MODEL: word2vec * * *
 [[   98620      526]
  [    9920 12807770]]
               precision    recall  f1-score   support
 
- not similar       1.00      1.00      1.00  12817690
-     similar       0.91      0.99      0.95     99146
+ not similar    0.99996   0.99923   0.99959  12817690
+     similar    0.90861   0.99469   0.94970     99146
 
-    accuracy                           1.00  12916836
-   macro avg       0.95      1.00      0.97  12916836
-weighted avg       1.00      1.00      1.00  12916836
+    accuracy                        0.99919  12916836
+   macro avg    0.95428   0.99696   0.97465  12916836
+weighted avg    0.99926   0.99919   0.99921  12916836
 
 * * * NEW MODEL: elmo * * *
 [[  99130      16]
  [6881546 5936144]]
               precision    recall  f1-score   support
 
- not similar       1.00      0.46      0.63  12817690
-     similar       0.01      1.00      0.03     99146
+ not similar    1.00000   0.46312   0.63306  12817690
+     similar    0.01420   0.99984   0.02800     99146
 
-    accuracy                           0.47  12916836
-   macro avg       0.51      0.73      0.33  12916836
-weighted avg       0.99      0.47      0.63  12916836
+    accuracy                        0.46724  12916836
+   macro avg    0.50710   0.73148   0.33053  12916836
+weighted avg    0.99243   0.46724   0.62841  12916836
 
 * * * NEW MODEL: roberta * * *
 [[   77628    21518]
  [   16476 12801214]]
               precision    recall  f1-score   support
 
- not similar       1.00      1.00      1.00  12817690
-     similar       0.82      0.78      0.80     99146
+ not similar    0.99832   0.99871   0.99852  12817690
+     similar    0.82492   0.78297   0.80339     99146
 
-    accuracy                           1.00  12916836
-   macro avg       0.91      0.89      0.90  12916836
-weighted avg       1.00      1.00      1.00  12916836
+    accuracy                        0.99706  12916836
+   macro avg    0.91162   0.89084   0.90096  12916836
+weighted avg    0.99699   0.99706   0.99702  12916836
 
 * * * NEW MODEL: use * * *
 [[   97324     1822]
  [     140 12817550]]
               precision    recall  f1-score   support
 
- not similar       1.00      1.00      1.00  12817690
-     similar       1.00      0.98      0.99     99146
+ not similar    0.99986   0.99999   0.99992  12817690
+     similar    0.99856   0.98162   0.99002     99146
 
-    accuracy                           1.00  12916836
-   macro avg       1.00      0.99      0.99  12916836
-weighted avg       1.00      1.00      1.00  12916836
+    accuracy                        0.99985  12916836
+   macro avg    0.99921   0.99081   0.99497  12916836
+weighted avg    0.99985   0.99985   0.99985  12916836
 """
 
 DEBUG = False
@@ -108,4 +108,4 @@ for model in ["levenshtein", "word2vec", "elmo", "roberta", "use"]:
                 
             
     print(confusion_matrix(actual, pred, labels=[1, 0]))
-    print(classification_report(actual, pred, target_names=['not similar', 'similar']))
+    print(classification_report(actual, pred, target_names=['not similar', 'similar'], digits=5))
--- scripts/comments_time_graphs.py
+++ scripts/comments_time_graphs.py
@@ -2,14 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 
-
 x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 y_generate = [1.22, 2.08, 3.04, 4.1, 5.23, 6.66, 7.43, 8.33, 9.32, 10.01]
 y_process = [254, 795, 1866, 3920, 5123, 7344, 9457, 11654, 14902, 17778]
 y_embs = [788, 1556, 2304, 3032, 3740, 4428, 5096, 5744, 6372, 6980]
 
 
-new_x = np.linspace(min(x), max(x), num=10000)
+new_x = np.linspace(min(x), max(x), num=1000)
 coefs_generate = np.polyfit(x,y_generate,1)
 new_line_generate = np.polyval(coefs_generate, new_x)
 
@@ -21,26 +20,36 @@ new_line_embs = np.polyval(coefs_embs, new_x)
 
 fig, axes = plt.subplots(1, 3, figsize=(15, 5))
 
-axes[0].scatter(x,y_generate)
-axes[0].scatter(new_x,new_line_generate,c='g', marker='^', s=5)
+axes[0].scatter(new_x,new_line_generate,c='r', marker='^', s=0.2)
+axes[0].scatter(x,y_generate, marker='X', s=48)
 axes[0].set_xlim(0,max(x)+1)
 axes[0].set_ylim(0,max(y_generate) * 110 / 100)
-axes[0].set_xlabel('r')
-axes[0].set_ylabel('time for processing USE embeddings (seconds)')
+axes[0].set_xlabel('r', fontsize=12)
+axes[0].set_ylabel('Time for generating USE embeddings (seconds)', fontsize=12)
+axes[0].minorticks_on()
+axes[0].xaxis.grid(True, which='major', linestyle='--')
+axes[0].yaxis.grid(True, which='major', linestyle='--')
+
 
 axes[1].scatter(x,y_process)
-axes[1].scatter(new_x,new_line_process,c='g', marker='^', s=5)
+axes[1].scatter(new_x,new_line_process,c='r', marker='^', s=0.2)
 axes[1].set_xlim(0,max(x)+1)
 axes[1].set_ylim(0,max(y_process) * 110 / 100)
-axes[1].set_xlabel('r')
-axes[1].set_ylabel('time for processing USE embeddings (seconds)')
+axes[1].set_xlabel('r', fontsize=12)
+axes[1].set_ylabel('Time for processing USE embeddings (seconds)', fontsize=12)
+axes[1].minorticks_on()
+axes[1].xaxis.grid(True, which='major', linestyle='--')
+axes[1].yaxis.grid(True, which='major', linestyle='--')
 
 axes[2].scatter(x,y_embs)
-axes[2].scatter(new_x,new_line_embs,c='g', marker='^', s=5)
+axes[2].scatter(new_x,new_line_embs,c='r', marker='^', s=0.2)
 axes[2].set_xlim(0,max(x)+1)
 axes[2].set_ylim(0,max(y_embs) * 110 / 100)
-axes[2].set_xlabel('r')
-axes[2].set_ylabel('number of embeddings generated')
+axes[2].set_xlabel('r', fontsize=12)
+axes[2].set_ylabel('Number of embeddings generated', fontsize=12)
+axes[2].minorticks_on()
+axes[2].xaxis.grid(True, which='major', linestyle='--')
+axes[2].yaxis.grid(True, which='major', linestyle='--')
 
 plt.tight_layout()
 plt.show()

b0bf58fb09fd23b42dd0eebaa9025c23b03ae93b
raresraf <raresfolea97@gmail.com>
2024-10-11 16:36:18 +0300

    experimental network

---
+++ experimental/capture_packets.py
@@ -0,0 +1,44 @@
+from scapy.all import *
+import os
+
+scenario_id = 1
+dump_id = 1
+db_type = "mysql_8_4"
+
+def packet_callback(packet):
+  """
+  Callback function to process both incoming and outgoing packets on port 3306, 
+  and display the hexdump of the application layer data.
+
+  Args:
+    packet: The intercepted packet.
+  """
+  
+  global dump_id
+  
+  if TCP in packet and (packet[TCP].sport == 3306 or packet[TCP].dport == 3306):
+    print("Packet Summary:")
+    print(packet.summary())
+
+    print("\nApplication Layer Hexdump:")
+    try:
+      app_layer_data = bytes(packet[TCP].payload)
+      if app_layer_data:
+        print(hexdump(app_layer_data))
+        path = f"/Users/raresraf/code/project-martial/experimental/network/{db_type}/scenarios/scenario_{scenario_id}/dump_{dump_id}.bin"
+        directory = os.path.dirname(path)
+        if directory:
+            os.makedirs(directory, exist_ok=True)
+        with open(path, "wb") as file:
+            file.write(app_layer_data)
+            dump_id = dump_id + 1
+      else:
+        print("No application layer data found.")
+    except Exception as e:
+      print(f"Error processing application layer data: {e}")
+    print("-" * 30)
+
+
+# Start sniffing packets on port 3306 (both incoming and outgoing)
+sniff(prn=packet_callback, filter="tcp port 3306")
+

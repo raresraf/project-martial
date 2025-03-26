408dff40e4ae4bf61df2df2ad7fd172c4d545a5a
Rares Folea <raresfolea97@gmail.com>
2025-03-26 20:16:40 +0200

    draft svm

---
+++ packets/classifier.py
@@ -0,0 +1,69 @@
+import os
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+import xgboost as xgb
+
+def load_byte_data(directory):
+    data = []
+    labels = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            filepath = os.path.join(root, file)
+            print(filepath)
+            try:
+                with open(filepath, 'rb') as f:
+                    byte_data = f.read()
+                    data.append(byte_data)
+                    engine = os.path.basename(root)
+                    label = "unknown"
+                    
+                    if "mysql" in engine.lower():
+                        label = "mysql"
+                    if "postgres" in engine.lower():
+                        label = "postgres"
+                    if "sqlserver" in engine.lower():
+                        label = "sqlserver"
+                    print(label)
+                    labels.append(label)
+            except Exception as e:
+                print(f"Error reading file {filepath}: {e}")
+    return data, labels
+
+
+if __name__ == "__main__":
+    print("Hello!")
+
+    train_dir = "/Users/raf/code/project-martial/dataset/packets/train"
+    train_data, train_labels = load_byte_data(train_dir)
+
+    test_dir = "/Users/raf/code/project-martial/dataset/packets/test"
+    test_data, test_labels = load_byte_data(test_dir)
+
+    def byte_ngram_tokenizer(byte_sequence):
+        ngrams = [byte_sequence[i:i + 2] for i in range(len(byte_sequence) - 2)]
+        return ngrams
+
+    # TF-IDF Vectorization
+    vectorizer = TfidfVectorizer(tokenizer=byte_ngram_tokenizer, encoding='cp437')
+    X_train = vectorizer.fit_transform(train_data)
+    X_test = vectorizer.transform(test_data)
+
+    # Train SVM classifier
+    classifier = SVC()
+    classifier.fit(X_train, train_labels)
+    predictions = classifier.predict(X_test)
+    accuracy = accuracy_score(test_labels, predictions)
+    print("Accuracy:", accuracy)
+
+
+    # Train XGBoost classifier
+    classifier = xgb.XGBClassifier()
+    classifier.fit(X_train, train_labels)
+    predictions = classifier.predict(X_test)
+    accuracy = accuracy_score(test_labels, predictions)
+    print("Accuracy:", accuracy)
+
+
+
